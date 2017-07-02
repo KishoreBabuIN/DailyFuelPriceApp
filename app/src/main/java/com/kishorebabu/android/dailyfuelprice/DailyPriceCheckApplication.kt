06ee3a0035d949505119e7c1b8796fd9592fe646
Kishore Babu <kishore@f22labs.com>
2017-07-02 15:00:53 +0530

    Add: Fabric integrated

--- app/src/main/java/com/kishorebabu/android/dailyfuelprice/DailyPriceCheckApplication.kt
+++ app/src/main/java/com/kishorebabu/android/dailyfuelprice/DailyPriceCheckApplication.kt
@@ -2,11 +2,14 @@ package com.kishorebabu.android.dailyfuelprice
 
 import android.content.Context
 import android.support.multidex.MultiDexApplication
+import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
-import com.squareup.leakcanary.LeakCanary
 import com.kishorebabu.android.dailyfuelprice.injection.component.ApplicationComponent
 import com.kishorebabu.android.dailyfuelprice.injection.component.DaggerApplicationComponent
 import com.kishorebabu.android.dailyfuelprice.injection.module.ApplicationModule
+import com.squareup.leakcanary.LeakCanary
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class DailyPriceCheckApplication : MultiDexApplication() {
@@ -16,10 +19,13 @@ class DailyPriceCheckApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
+        Fabric.with(this, Crashlytics())
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
             Stetho.initializeWithDefaults(this)
             LeakCanary.install(this)
+        } else {
+            Timber.plant(CrashReportingTree())
         }
     }
 
@@ -42,5 +48,20 @@ class DailyPriceCheckApplication : MultiDexApplication() {
         operator fun get(context: Context): DailyPriceCheckApplication {
             return context.applicationContext as DailyPriceCheckApplication
         }
+    }
+}
+
+private class CrashReportingTree : Timber.Tree() {
+
+    override fun log(priority: Int, tag: String, message: String, t: Throwable?) {
+        if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+            return
+        }
+
+        if (t == null)
+            Crashlytics.log(priority, tag, message)
+        else
+            Crashlytics.logException(t)
+
     }
 }
